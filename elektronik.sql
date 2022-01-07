450c31bfdb6c0d105fe9e1ebc07b2c23aafa0597
RafliTelyuu <93783255+RafliTelyuu@users.noreply.github.com>
2022-01-08 00:07:25 +0700

    Add files via upload

---
+++ elektronik.sql
@@ -0,0 +1,181 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 07, 2022 at 05:56 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `elektronik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_barang`
+--
+
+CREATE TABLE `tbl_barang` (
+  `kd_barang` varchar(10) NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `jumlah_barang` int(11) NOT NULL,
+  `harga_beli` int(11) NOT NULL,
+  `harga_jual` int(11) NOT NULL,
+  `tanggal_masuk` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_barang`
+--
+
+INSERT INTO `tbl_barang` (`kd_barang`, `nama_barang`, `jumlah_barang`, `harga_beli`, `harga_jual`, `tanggal_masuk`) VALUES
+('B0001', 'kulkas', 96, 1000000, 1500000, '2018-05-15'),
+('B0002', 'setrika', 29, 500000, 670000, '2018-05-15'),
+('B0003', 'televisi', 15, 1400000, 2300000, '2018-05-15'),
+('B0004', 'kipas angin', 21, 4000000, 4750000, '2018-05-15'),
+('B0005', 'mesin cuci', 10, 4000000, 5780000, '2018-05-15'),
+('B0006', 'Ilo', 95, 20000, 10000, '2022-01-07');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_beli`
+--
+
+CREATE TABLE `tbl_beli` (
+  `nofaktur` varchar(11) NOT NULL,
+  `kd_barang` varchar(11) NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `hsatuan` int(11) NOT NULL,
+  `jumlah_beli` int(11) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `bayar` int(11) NOT NULL,
+  `kembalian` int(11) NOT NULL,
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_beli`
+--
+
+INSERT INTO `tbl_beli` (`nofaktur`, `kd_barang`, `nama_barang`, `hsatuan`, `jumlah_beli`, `harga`, `bayar`, `kembalian`, `tanggal`) VALUES
+('F0001', 'B0002', 'setrika', 670000, 1, 670000, 6000000, 730000, '2018-05-15'),
+('F0001', 'B0003', 'televisi', 2300000, 2, 4600000, 6000000, 730000, '2018-05-15'),
+('F0002', 'B0005', 'mesin cuci', 5780000, 2, 11560000, 20000000, 40000, '2018-05-15'),
+('F0002', 'B0001', 'kulkas', 1500000, 1, 1500000, 20000000, 40000, '2018-05-15'),
+('F0002', 'B0003', 'televisi', 2300000, 3, 6900000, 20000000, 40000, '2018-05-15'),
+('F0003', 'B0001', 'kulkas', 1500000, 1, 1500000, 3000000, 1500000, '2022-01-07'),
+('F0004', 'B0001', 'kulkas', 1500000, 1, 1500000, 3000000, 1500000, '2022-01-07'),
+('F0005', 'B0006', 'Ilo', 10000, 2, 20000, 50000, 30000, '2022-01-07'),
+('F0006', 'B0001', 'kulkas', 1500000, 1, 1500000, 2000000, 500000, '2022-01-07'),
+('F0007', 'B0006', 'Ilo', 10000, 2, 20000, 50000, 30000, '2022-01-07');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_login`
+--
+
+CREATE TABLE `tbl_login` (
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `jenis_kelamin` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `no_telp` int(30) NOT NULL,
+  `agama` varchar(30) NOT NULL,
+  `alamat` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_login`
+--
+
+INSERT INTO `tbl_login` (`username`, `password`, `jenis_kelamin`, `email`, `no_telp`, `agama`, `alamat`) VALUES
+('ardiansa', '123', 'Laki-Laki', 'ardiansa@gmail.com', 8123, 'Islam', 'Cengkareng'),
+('ilo', '123', 'Laki-Laki', 'ilo@gmail.com', 8, 'Islam', 'Papua'),
+('kelvin', '123', 'Laki-Laki', 'kelvin@gmail.com', 82, 'Islam', 'Batang'),
+('rafli', '123', 'Perempuan', 'adwdaw', 12, 'Islam', 'juah'),
+('rama', '123', 'Laki-Laki', 'rama@gmail.com', 8123, 'Islam', 'Aceh');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_tmp_beli`
+--
+
+CREATE TABLE `tbl_tmp_beli` (
+  `id_tmp` int(11) NOT NULL,
+  `kd_barang` varchar(11) NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `hsatuan` int(11) NOT NULL,
+  `jumlah_beli` int(11) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Triggers `tbl_tmp_beli`
+--
+DELIMITER $$
+CREATE TRIGGER `batal` AFTER DELETE ON `tbl_tmp_beli` FOR EACH ROW BEGIN
+UPDATE tbl_barang SET jumlah_barang = jumlah_barang + OLD.jumlah_beli
+WHERE kd_barang = OLD.kd_barang;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `beli` AFTER INSERT ON `tbl_tmp_beli` FOR EACH ROW BEGIN 
+UPDATE tbl_barang SET jumlah_barang = jumlah_barang - new.jumlah_beli 
+WHERE kd_barang = new.`kd_barang`; 
+END
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_barang`
+--
+ALTER TABLE `tbl_barang`
+  ADD PRIMARY KEY (`kd_barang`);
+
+--
+-- Indexes for table `tbl_login`
+--
+ALTER TABLE `tbl_login`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `tbl_tmp_beli`
+--
+ALTER TABLE `tbl_tmp_beli`
+  ADD PRIMARY KEY (`id_tmp`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_tmp_beli`
+--
+ALTER TABLE `tbl_tmp_beli`
+  MODIFY `id_tmp` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
